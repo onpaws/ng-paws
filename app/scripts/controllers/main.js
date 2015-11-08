5547ae86b04a165f2e5621ddb594480910573762
paws <pas8314@gmail.com>
2015-11-07 19:20:08 -0500

    yeoman-generated angular 1.3.15 scaffold

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngPawsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngPawsApp
+ */
+angular.module('ngPawsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
